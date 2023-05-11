009c5bee505695118bb5eb81392cc5591cbc1ff8
Jayklef <jerryarhawho@gmail.com>
2023-05-11 15:10:12 +0100

    Added contact form html

--- src/main/java/com/jayklef/contactdirectory/controller/ContactController.java
+++ src/main/java/com/jayklef/contactdirectory/controller/ContactController.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
 
@@ -22,10 +23,20 @@ public class ContactController {
         model.addAttribute("contacts", contacts);
         return "contacts";
     }
+
+    @GetMapping("addContactForm")
+    public String saveContact(Model model){
+        model.addAttribute("contact", new Contact());
+        return "contact-form";
+    }
+
+
     @PostMapping("/save")
-    public String saveContact(Contact contact){
+    public String saveContact(Contact contact, RedirectAttributes attributes){
+        String saveInfo = "Contact saved successfully";
         contactService.saveContact(contact);
-        return "contact";
+        attributes.addFlashAttribute("message", saveInfo);
+        return "redirect:/contact";
     }
 
 }
